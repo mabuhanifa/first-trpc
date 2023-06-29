f6492d3bdb99d62734a74924bab0b798ab8d7545
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-29 23:23:57 +0600

    trpc router

--- packages/api-server/index.ts
+++ packages/api-server/index.ts
@@ -1,3 +1,5 @@
+import { initTRPC } from "@trpc/server";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import express from "express";
 
 const app = express();
@@ -6,6 +8,26 @@ const port = 8080;
 app.get("/", (req, res) => {
   res.send("Hello from api-server");
 });
+
+const t = initTRPC.create();
+
+const appRouter = t.router({
+  sayHi: t.procedure.query(() => {
+    return "hi";
+  }),
+
+  logToServer: t.procedure.input(String).mutation((req) => {
+    console.log(req.input);
+    return true;
+  }),
+});
+
+app.use(
+  "/trpc",
+  createExpressMiddleware({
+    router: appRouter,
+  })
+);
 
 app.listen(port, () => {
   console.log(`api-server listening at http://localhost:${port}`);
